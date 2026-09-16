9498caecf60f701648cb404309136657d4b559b6
Maker <16922281+choutuo@user.noreply.gitee.com>
2026-09-16 14:17:18 +0800

    [feature/update]:更新

--- ctrpc-rpc/src/main/java/com/ctrpc/rpc/registry/StaticServiceRegistry.java
+++ ctrpc-rpc/src/main/java/com/ctrpc/rpc/registry/StaticServiceRegistry.java
@@ -1,12 +1,19 @@
 package com.ctrpc.rpc.registry;
+
 import com.ctrpc.rpc.config.RpcProperties;
+
 import java.util.*;
+
 public class StaticServiceRegistry implements ServiceRegistry {
- private final RpcProperties properties;
- public StaticServiceRegistry(RpcProperties properties){this.properties=properties;}
- public List<ServiceMeta> discover(String serviceName){
-   RpcProperties.ServiceDependency d=properties.getDependencies().get(serviceName);
-   if(d==null||d.getAddress()==null) return Collections.emptyList();
-   return Collections.singletonList(new ServiceMeta(serviceName,d.getAddress()));
- }
+    private final RpcProperties properties;
+
+    public StaticServiceRegistry(RpcProperties properties) {
+        this.properties = properties;
+    }
+
+    public List<ServiceMeta> discover(String serviceName) {
+        RpcProperties.ServiceDependency d = properties.getDependencies().get(serviceName);
+        if (d == null || d.getAddress() == null) return Collections.emptyList();
+        return Collections.singletonList(new ServiceMeta(serviceName, d.getAddress()));
+    }
 }
