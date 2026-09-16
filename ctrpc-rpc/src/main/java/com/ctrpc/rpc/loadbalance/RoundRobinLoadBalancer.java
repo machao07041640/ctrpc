0da194e29436c15d1724f23672109553d9970f72
Maker <16922281+choutuo@user.noreply.gitee.com>
2026-09-16 09:42:54 +0800

    [master]:格式

--- ctrpc-rpc/src/main/java/com/ctrpc/rpc/loadbalance/RoundRobinLoadBalancer.java
+++ ctrpc-rpc/src/main/java/com/ctrpc/rpc/loadbalance/RoundRobinLoadBalancer.java
@@ -1,11 +1,15 @@
 package com.ctrpc.rpc.loadbalance;
+
 import com.ctrpc.rpc.registry.ServiceMeta;
+
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+
 public class RoundRobinLoadBalancer implements LoadBalancer {
- private final AtomicInteger index=new AtomicInteger();
- public ServiceMeta select(List<ServiceMeta> nodes){
-  if(nodes==null||nodes.isEmpty()) throw new IllegalStateException("No RPC instances");
-  return nodes.get(Math.abs(index.getAndIncrement())%nodes.size());
- }
+    private final AtomicInteger index = new AtomicInteger();
+
+    public ServiceMeta select(List<ServiceMeta> nodes) {
+        if (nodes == null || nodes.isEmpty()) throw new IllegalStateException("No RPC instances");
+        return nodes.get(Math.abs(index.getAndIncrement()) % nodes.size());
+    }
 }
