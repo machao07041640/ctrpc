729132aafbdc70b88034c98fb213ad812e170e14
machao07041640 <59259749+machao07041640@users.noreply.github.com>
2026-09-22 08:23:46 +0800

    feat(rpc): add pluggable exception resolver contract

---
+++ ctrpc-rpc/src/main/java/com/ctrpc/rpc/exception/RpcError.java
@@ -0,0 +1,20 @@
+package com.ctrpc.rpc.exception;
+
+public class RpcError {
+
+    private final int code;
+    private final String message;
+
+    public RpcError(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
